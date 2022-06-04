9a2791304748ae8fdb72fb3c0ad2f7c64feb1fb4
Yunya Hsu <yunyahsu110@gmail.com>
2022-06-04 12:56:35 +0800

    feat: add cookie & session

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 // 引入外部套件
 const express = require('express')
 const exphbs = require('express-handlebars')
+const session = require('express-session')
 
 // 建立基本參數
 const port = 3000
@@ -9,6 +10,14 @@ let validatedUser = ''
 
 // 使用者名單
 const users = require('./models/users.json')
+
+// 建立 session 的 middleware，並將需要帶入的屬性放進 options 物件中
+app.use(session({
+ secret: 'mySecret', // 存放在cookie的sessionID
+ name: 'isLoggedIn', // name是指存放在cookie的key，可以不寫，如果不寫的話預設是connect.sid
+ resave: false, // 代表在每次與使用者互動後，不會強制把 session 儲存，除非 session 有變動
+ saveUninitialized: false
+}))
 
 // 使用express-handlebars為樣板引擎
 app.engine('handlebars', exphbs({ defaultLayout: 'main' }))
@@ -34,6 +43,10 @@ app.post('/', (req, res) => {
       res.render('index', { errorPassword })
     } else {
       validatedUser = validateEmail.firstName
+      req.session.user = validateEmail.firstName
+      req.session.isLoggedIn = true  // 在 session物件上新增key和value
+      console.log(req.session)
+      console.log(req.sessionID)
       res.redirect('/login')
     }
   }
